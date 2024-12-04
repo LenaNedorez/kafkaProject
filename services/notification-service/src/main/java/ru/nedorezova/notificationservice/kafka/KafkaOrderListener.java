3a537b8ca6dcedaa416a603ef1f10eff503069b0
Elena Nedorezova <nedorezova.emp@gmail.com>
2024-12-04 10:02:59 +0300

    fixing failing listener

--- services/notification-service/src/main/java/ru/nedorezova/notificationservice/kafka/KafkaOrderListener.java
+++ services/notification-service/src/main/java/ru/nedorezova/notificationservice/kafka/KafkaOrderListener.java
@@ -1,6 +1,7 @@
 package ru.nedorezova.notificationservice.kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import ru.nedorezova.notificationservice.entity.Order;
 
@@ -8,7 +9,7 @@ import ru.nedorezova.notificationservice.entity.Order;
 public class KafkaOrderListener {
 
     @KafkaListener(topics = "order-topic")
-    public void sendConfirmationOrderToEmail(Order order) {
-        System.out.println(order);
+    public void sendConfirmationOrderToEmail(Message<Order> orderMessage) {
+        System.out.println(orderMessage.getPayload());
     }
 }
